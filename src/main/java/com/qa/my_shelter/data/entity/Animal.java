a33b039b31279b4061ed0bbf8236191ab636f535
Lee <leenock2012@yahoo.co.uk>
2022-04-25 12:54:08 +0100

    Update Animal.java
    Added getters and setters + added an all args constructor

--- src/main/java/com/qa/my_shelter/data/entity/Animal.java
+++ src/main/java/com/qa/my_shelter/data/entity/Animal.java
@@ -34,4 +34,57 @@ public class Animal {
 	
 	@NotNull
 	private Date entry_date;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSpecies() {
+		return species;
+	}
+
+	public void setSpecies(String species) {
+		this.species = species;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public Date getEntry_date() {
+		return entry_date;
+	}
+
+	public void setEntry_date(Date entry_date) {
+		this.entry_date = entry_date;
+	}
+
+	public Animal(int id,
+			@NotNull @NotBlank @Size(min = 2, max = 20, message = "The animal's name must have at least two characters and a maximum of twenty") String name,
+			@NotNull String species, @NotNull String gender, @NotNull Date entry_date) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.species = species;
+		this.gender = gender;
+		this.entry_date = entry_date;
+	}
+	
+	
 }
