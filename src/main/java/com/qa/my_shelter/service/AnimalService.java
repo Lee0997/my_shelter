2828ce4e739a73e356df662f999dec3b976a1b17
Lee <leenock2012@yahoo.co.uk>
2022-04-25 13:26:16 +0100

    Update AnimalService.java
    Added some of the necessary features to the service.

--- src/main/java/com/qa/my_shelter/service/AnimalService.java
+++ src/main/java/com/qa/my_shelter/service/AnimalService.java
@@ -1,5 +1,22 @@
 package com.qa.my_shelter.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.qa.my_shelter.data.repository.AnimalRepository;
+
+import org.modelmapper.ModelMapper;
+
+@Service
 public class AnimalService {
 
+	private AnimalRepository animalRepository;
+	private ModelMapper modelMapper;
+	
+	@Autowired
+	public AnimalService(AnimalRepository animalRepository, ModelMapper modelMapper) {
+		super();
+		this.animalRepository = animalRepository;
+		this.modelMapper = modelMapper;
+	}
 }
