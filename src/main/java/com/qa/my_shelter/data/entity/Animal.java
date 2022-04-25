93df05c590f5ee059d764a1d27d0dc021d2c40fe
Lee <leenock2012@yahoo.co.uk>
2022-04-25 12:35:25 +0100

    Update Animal.java
    Added the necessary fields to the entity

--- src/main/java/com/qa/my_shelter/data/entity/Animal.java
+++ src/main/java/com/qa/my_shelter/data/entity/Animal.java
@@ -1,5 +1,37 @@
 package com.qa.my_shelter.data.entity;
 
-public class Animal {
+import java.sql.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "animal")
+public class Animal {
+	
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@NotNull
+	@NotBlank
+	@Size(min = 2, max = 20, message = "The animal's name must have at least two characters and a maximum of twenty")
+	private String name;
+	
+	@NotNull
+	private String species;
+	
+	@NotNull
+	private String gender;
+	
+	@NotNull
+	private Date entry_date;
 }
