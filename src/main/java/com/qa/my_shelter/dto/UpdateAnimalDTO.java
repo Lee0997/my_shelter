5bdd3592a3e2447f63b39bc5e52dee4e546bab3b
Lee <leenock2012@yahoo.co.uk>
2022-04-26 11:22:16 +0100

    Update UpdateAnimalDTO.java
    Created the UpdateAnimalDTO for Post Mapping

--- src/main/java/com/qa/my_shelter/dto/UpdateAnimalDTO.java
+++ src/main/java/com/qa/my_shelter/dto/UpdateAnimalDTO.java
@@ -1,5 +1,80 @@
 package com.qa.my_shelter.dto;
 
+import java.util.Objects;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 public class UpdateAnimalDTO {
 
+	private int id;
+	
+	@NotNull
+	@NotBlank
+	private String name;
+	
+	@NotNull
+	@NotBlank
+	private String species;
+	
+	@NotNull
+	@NotBlank
+	private String gender;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSpecies() {
+		return species;
+	}
+
+	public void setSpecies(String species) {
+		this.species = species;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(gender, id, name, species);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UpdateAnimalDTO other = (UpdateAnimalDTO) obj;
+		return Objects.equals(gender, other.gender) && id == other.id && Objects.equals(name, other.name)
+				&& Objects.equals(species, other.species);
+	}
+
+	@Override
+	public String toString() {
+		return "UpdateAnimalDTO [id=" + id + ", name=" + name + ", species=" + species + ", gender=" + gender + "]";
+	}
+	
+	
 }
