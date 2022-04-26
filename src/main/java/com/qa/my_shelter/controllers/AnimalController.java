4fe488cb162c12ce6e53b9523f2cb69f59f591a4
Lee <leenock2012@yahoo.co.uk>
2022-04-26 11:14:21 +0100

    Update AnimalController.java
    Create Post Mapping

--- src/main/java/com/qa/my_shelter/controllers/AnimalController.java
+++ src/main/java/com/qa/my_shelter/controllers/AnimalController.java
@@ -2,14 +2,21 @@ package com.qa.my_shelter.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.qa.my_shelter.service.*;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 import com.qa.my_shelter.dto.*;
 
 @RestController
@@ -34,5 +41,13 @@ public class AnimalController {
 		return new ResponseEntity<>(animal, HttpStatus.OK);
 	}
 	
-	
+	@PostMapping
+	public ResponseEntity<AnimalDTO> createAnimal(@Valid @RequestBody NewAnimalDTO animal) {
+		AnimalDTO newAnimal = animalService.createAnimal(animal);
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Location", "http://localhost:8080/animal/" + newAnimal.getId());
+		
+		return new ResponseEntity<>(newAnimal, headers, HttpStatus.CREATED);
+	}
 }
