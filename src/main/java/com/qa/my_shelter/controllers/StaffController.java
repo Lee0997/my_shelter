2e53dfb3228e473cf17dd36d729d82203bcd755b
Lee <leenock2012@yahoo.co.uk>
2022-04-26 10:00:50 +0100

    Update StaffController.java
    Began work on post mapping

--- src/main/java/com/qa/my_shelter/controllers/StaffController.java
+++ src/main/java/com/qa/my_shelter/controllers/StaffController.java
@@ -2,15 +2,21 @@ package com.qa.my_shelter.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.qa.my_shelter.dto.NewStaffDTO;
 import com.qa.my_shelter.dto.StaffDTO;
 import com.qa.my_shelter.service.StaffService;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping(path = "/post")
@@ -31,5 +37,10 @@ public class StaffController {
 	@GetMapping(path = "/{id}")
 	public ResponseEntity<StaffDTO> getStaff(@PathVariable(name = "id") int id) {
 		return ResponseEntity.ok(staffService.getStaffById(id));
+	}
+	
+	@PostMapping
+	public ResponseEntity<StaffDTO> createStaff(@Valid @RequestBody NewStaffDTO staff) {
+		StaffDTO newStaff = staffService.createStaff(staff);
 	}
 }
