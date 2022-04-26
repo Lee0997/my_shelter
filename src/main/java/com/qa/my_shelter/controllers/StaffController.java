2ec4c406566cce9387780b939c60197a72ab602d
Lee <leenock2012@yahoo.co.uk>
2022-04-26 09:55:46 +0100

    Update StaffController.java
    Set up and added get mapping

--- src/main/java/com/qa/my_shelter/controllers/StaffController.java
+++ src/main/java/com/qa/my_shelter/controllers/StaffController.java
@@ -1,5 +1,30 @@
 package com.qa.my_shelter.controllers;
 
-public class StaffController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.qa.my_shelter.dto.StaffDTO;
+import com.qa.my_shelter.service.StaffService;
+
+@RestController
+@RequestMapping(path = "/post")
+public class StaffController {
+	
+	private StaffService staffService;
+	
+	@Autowired
+	public StaffController(StaffService staffService) {
+		this.staffService = staffService;
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<StaffDTO>> getStaff() {
+		return ResponseEntity.ok(staffService.getStaff());
+	}
+	
 }
