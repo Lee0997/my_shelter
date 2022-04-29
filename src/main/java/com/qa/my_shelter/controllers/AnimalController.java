3bcc89079efe4440a7e7ef867e25ab314c922613
Lee <leenock2012@yahoo.co.uk>
2022-04-29 16:00:39 +0100

    Added docs

--- src/main/java/com/qa/my_shelter/controllers/AnimalController.java
+++ src/main/java/com/qa/my_shelter/controllers/AnimalController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.qa.my_shelter.service.*;
 import com.qa.my_shelter.dto.*;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping(path = "/animal")
 public class AnimalController {
